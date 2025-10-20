c6348d2a22dbed0d0425809000e9fd1442aa680d
thereclaimer <sam.davenport@thereclaimergame.com>
2025-10-20 15:17:39 -0500

    more config changes

--- engine/src/asset/ifb-engine-asset-config.cpp
+++ engine/src/asset/ifb-engine-asset-config.cpp
@@ -2,114 +2,205 @@
 
 #include "ifb-engine-asset-config.hpp"
 
-namespace ifb {
+namespace ifb::eng {
 
-    IFB_ENG_INTERNAL eng_asset_config_editor_context_t*
-    asset_config_editor_context_init(
+    IFB_ENG_INTERNAL asset_config_editor_context_t*
+    asset_config_editor_arena_alloc(
         eng_mem_arena_t* arena) {
 
-        assert(arena);
+        auto config       = sld::arena_push_struct<asset_config_editor_context_t>(arena);
+        auto path         = sld::arena_push_struct<eng_asset_config_cstr_t>(arena);
+        auto list_text    = sld::arena_push_struct<asset_config_list_t>(arena);
+        auto list_image   = sld::arena_push_struct<asset_config_list_t>(arena);
+        auto list_sound   = sld::arena_push_struct<asset_config_list_t>(arena);
+        auto list_font    = sld::arena_push_struct<asset_config_list_t>(arena);
+        auto list_deleted = sld::arena_push_struct<asset_config_list_t>(arena);
 
-        auto config = sld::arena_push_struct<eng_asset_config_editor_context_t>(arena);
-        assert(config);
+        const bool can_init = (
+            config       != NULL &&
+            path         != NULL &&
+            list_text    != NULL &&
+            list_image   != NULL &&
+            list_sound   != NULL &&
+            list_font    != NULL &&
+            list_deleted != NULL
+        );
+        assert(can_init);
 
+        config->path              = path; 
+        config->node_list.text    = list_text;
+        config->node_list.image   = list_image;
+        config->node_list.sound   = list_sound;
+        config->node_list.font    = list_font;
+        config->node_list.deleted = list_deleted;
         return(config);
     }
 
-    IFB_ENG_INTERNAL eng_asset_config_file_node_t*
+    IFB_ENG_INTERNAL bool
+    asset_config_editor_context_is_valid(
+        const asset_config_editor_context_t* editor_context) {
+
+        const bool is_valid = (
+            editor_context                    != NULL &&
+            editor_context->path              != NULL &&
+            editor_context->node_list.text    != NULL &&
+            editor_context->node_list.image   != NULL &&
+            editor_context->node_list.sound   != NULL &&
+            editor_context->node_list.font    != NULL &&            
+            editor_context->node_list.deleted != NULL            
+        );
+        return(is_valid);
+    }
+
+    IFB_ENG_INTERNAL void
+    asset_config_editor_context_assert_valid(
+        const asset_config_editor_context_t* editor_context) {
+
+        assert(asset_config_editor_context_is_valid(editor_context));
+    }
+
+
+    IFB_ENG_INTERNAL asset_config_node_t*
     asset_config_editor_context_add_text(
-        eng_asset_config_editor_context_t* editor_context,
+        asset_config_editor_context_t* editor_context,
         eng_mem_arena_t*                   arena,
         const cchar*                       asset_name,
         const cchar*                       asset_path) {
 
-        assert(editor_context);
+        asset_config_editor_context_assert_valid(editor_context);
 
-        eng_asset_config_file_node_t* node = asset_config_arena_alloc_and_init_file_node(
+        asset_config_node_t* node = asset_config_arena_alloc_and_init_file_node(
             arena,
             asset_name,
             asset_path
         );
 
         asset_config_file_list_add_node(
-            &editor_context->file_list.text,
-            text_node);
+            editor_context->node_list.text,
+            node);
     }
 
-    IFB_ENG_INTERNAL eng_asset_config_file_node_t*
+    IFB_ENG_INTERNAL asset_config_node_t*
     asset_config_editor_context_add_image(
-        eng_asset_config_editor_context_t* editor_context,
+        asset_config_editor_context_t* editor_context,
         eng_mem_arena_t*                   arena,
         const cchar*                       asset_name,
         const cchar*                       asset_path) {
 
-        assert(editor_context);
+        asset_config_editor_context_assert_valid(editor_context);
 
-        eng_asset_config_file_node_t* node = asset_config_arena_alloc_and_init_file_node(
+        asset_config_node_t* node = asset_config_arena_alloc_and_init_file_node(
             arena,
             asset_name,
             asset_path
         );
 
         asset_config_file_list_add_node(
-            &editor_context->file_list.image,
-            text_node);
+            editor_context->node_list.image,
+            node);
     }
 
-    IFB_ENG_INTERNAL eng_asset_config_file_node_t*
+    IFB_ENG_INTERNAL asset_config_node_t*
     asset_config_editor_context_add_sound(
-        eng_asset_config_editor_context_t* editor_context,
+        asset_config_editor_context_t* editor_context,
         eng_mem_arena_t*                   arena,
         const cchar*                       asset_name,
         const cchar*                       asset_path) {
 
         assert(editor_context);
 
-        eng_asset_config_file_node_t* node = asset_config_arena_alloc_and_init_file_node(
+        asset_config_node_t* node = asset_config_arena_alloc_and_init_file_node(
             arena,
             asset_name,
             asset_path
         );
         
-        asset_config_file_list_add_node(&editor_context->file_list.sound, text_node);
+        asset_config_file_list_add_node(
+            editor_context->node_list.sound,
+            node);
     }
 
-    IFB_ENG_INTERNAL eng_asset_config_file_node_t*
+    IFB_ENG_INTERNAL asset_config_node_t*
     asset_config_editor_context_add_font(
-        eng_asset_config_editor_context_t* editor_context,
+        asset_config_editor_context_t* editor_context,
         eng_mem_arena_t*                   arena,
         const cchar*                       asset_name,
         const cchar*                       asset_path) {
 
-        assert(editor_context);
+        asset_config_editor_context_assert_valid(editor_context);
 
-        eng_asset_config_file_node_t* node = asset_config_arena_alloc_and_init_file_node(
+        asset_config_node_t* node = asset_config_arena_alloc_and_init_file_node(
             arena,
             asset_name,
             asset_path
         );
         
-        asset_config_file_list_add_node(&editor_context->file_list.font, text_node);
+        asset_config_file_list_add_node(
+            editor_context->node_list.font,
+            node);
     }
 
     IFB_ENG_INTERNAL void
     asset_config_editor_context_update_file_node_name(
-        eng_asset_config_file_node_t* file_node,
-        const cchar*                  asset_name) {
+        asset_config_editor_context_t* editor_context,
+        asset_config_node_t*           file_node,
+        const cchar*                       asset_name) {
 
+        asset_config_list_t* list = file_node != NULL ? (asset_config_list_t*)file_node->list : NULL;
+
+        const bool is_valid = (
+            asset_config_editor_context_is_valid(editor_context) &&
+            editor_context == list->editor_context
+        );
+        assert(is_valid);
+
+        asset_config_node_update_name(file_node, asset_name);
     }
 
     IFB_ENG_INTERNAL void
     asset_config_editor_context_update_file_node_path(
-        eng_asset_config_file_node_t* file_node,
-        const cchar*                  asset_path) {
+        asset_config_editor_context_t* editor_context,
+        asset_config_node_t*      file_node,
+        const cchar*                       asset_path) {
 
+        asset_config_list_t* list = file_node != NULL ? (asset_config_list_t*)file_node->list : NULL;
+
+        const bool is_valid = (
+            asset_config_editor_context_is_valid(editor_context) &&
+            editor_context == list->editor_context
+        );
+        assert(is_valid);
+
+        asset_config_node_update_path(file_node, asset_path);
     }
 
     IFB_ENG_INTERNAL void
     asset_config_editor_delete_file_node(
-        eng_asset_config_file_node_t* file_node) {
+        asset_config_editor_context_t* editor_context,
+        asset_config_node_t*      file_node) {
 
+        asset_config_list_t* list = file_node != NULL ? (asset_config_list_t*)file_node->list : NULL;
+
+        const bool is_valid = (
+            asset_config_editor_context_is_valid(editor_context) &&
+            editor_context == list->editor_context
+        );
+        assert(is_valid);
+
+        asset_config_node_t* prev = (asset_config_node_t*)file_node->prev;
+        asset_config_node_t* next = (asset_config_node_t*)file_node->next;
+        const bool is_first = (file_node == list->head);
+        const bool is_last  = (file_node == list->tail); 
+
+        file_node->prev = NULL;
+        file_node->next = NULL;
+
+        if (is_first) list->head = next;
+        if (is_last)  list->tail = prev;
+        if (next)     next->prev = prev;
+        if (prev)     prev->next = next;
+
+        asset_config_file_list_add_node(list, file_node);
     }
 
 };
--- engine/src/asset/ifb-engine-asset-config.hpp
+++ engine/src/asset/ifb-engine-asset-config.hpp
@@ -2,17 +2,20 @@
 #define IFB_ENGINE_ASSET_CONFIG_HPP
 
 #include <sld-arena.hpp>
+#include <sld-double-linked-list.hpp>
 #include "ifb-engine.hpp"
 #include "ifb-engine-asset.hpp"
 #include "ifb-engine-memory-internal.hpp"
 
-namespace ifb {
+using namespace ifb;
+namespace ifb::eng {
 
-    constexpr cchar ENG_ASSET_CONFIG_NODE_NAME_TEXT  [] = "text";
-    constexpr cchar ENG_ASSET_CONFIG_NODE_NAME_IMAGE [] = "image";
-    constexpr cchar ENG_ASSET_CONFIG_NODE_NAME_SOUND [] = "sound";
-    constexpr cchar ENG_ASSET_CONFIG_NODE_NAME_FONT  [] = "font";
-    constexpr cchar ENG_ASSET_CONFIG_DEFAULT_CONFIG  [] = 
+    constexpr u32   ASSET_CONFIG_CSTR_LENGTH        = 32;
+    constexpr cchar ASSET_CONFIG_NODE_NAME_TEXT  [] = "text";
+    constexpr cchar ASSET_CONFIG_NODE_NAME_IMAGE [] = "image";
+    constexpr cchar ASSET_CONFIG_NODE_NAME_SOUND [] = "sound";
+    constexpr cchar ASSET_CONFIG_NODE_NAME_FONT  [] = "font";
+    constexpr cchar ASSET_CONFIG_DEFAULT_CONFIG  [] = 
         "<ifb-assets>"
             "<text path=\"../assets/text\"></text>"
             "<image path=\"../assets/image\"></image>"
@@ -20,67 +23,74 @@ namespace ifb {
             "<font path=\"../assets/font\"></font>"
         "</ifb-assets>";
 
-    struct eng_asset_config_cstr_t;
-    struct eng_asset_config_file_t;
-    struct eng_asset_config_file_node_t;
-    struct eng_asset_config_editor_context_t;
+    struct asset_config_cstr_t;
+    struct asset_config_list_t;
+    struct asset_config_node_t;
+    struct asset_config_node_data_t;
+    struct asset_config_editor_context_t;
 
-    IFB_ENG_INTERNAL eng_asset_config_editor_context_t* asset_config_editor_context_init                  (eng_mem_arena_t* arena);
-    IFB_ENG_INTERNAL eng_asset_config_file_node_t*      asset_config_editor_context_add_text              (eng_asset_config_editor_context_t* editor_context, eng_mem_arena_t* arena, const cchar* asset_name, const cchar* asset_path);
-    IFB_ENG_INTERNAL eng_asset_config_file_node_t*      asset_config_editor_context_add_image             (eng_asset_config_editor_context_t* editor_context, eng_mem_arena_t* arena, const cchar* asset_name, const cchar* asset_path);
-    IFB_ENG_INTERNAL eng_asset_config_file_node_t*      asset_config_editor_context_add_sound             (eng_asset_config_editor_context_t* editor_context, eng_mem_arena_t* arena, const cchar* asset_name, const cchar* asset_path);
-    IFB_ENG_INTERNAL eng_asset_config_file_node_t*      asset_config_editor_context_add_font              (eng_asset_config_editor_context_t* editor_context, eng_mem_arena_t* arena, const cchar* asset_name, const cchar* asset_path);
-    IFB_ENG_INTERNAL void                               asset_config_editor_context_update_file_node_name (eng_asset_config_file_node_t*      file_node,      const cchar*     asset_name);
-    IFB_ENG_INTERNAL void                               asset_config_editor_context_update_file_node_path (eng_asset_config_file_node_t*      file_node,      const cchar*     asset_path);
-    IFB_ENG_INTERNAL void                               asset_config_editor_delete_file_node              (eng_asset_config_file_node_t*      file_node);
+    IFB_ENG_INTERNAL asset_config_cstr_t*           asset_config_cstr_arena_alloc                     (eng_mem_arena_t* arena);
+    IFB_ENG_INTERNAL void                           asset_config_cstr_update                          (asset_config_cstr_t* config_cstr, const cchar* value);
 
-    struct eng_asset_config_file_t {
+    IFB_ENG_INTERNAL asset_config_node_t*           asset_config_node_arena_alloc                     (eng_mem_arena_t* arena);
+    IFB_ENG_INTERNAL void                           asset_config_node_init                            (asset_config_node_t* node, const cchar* name, const cchar* path);
+    IFB_ENG_INTERNAL void                           asset_config_node_update_name                     (asset_config_node_t* node, const cchar* name);
+    IFB_ENG_INTERNAL void                           asset_config_node_update_path                     (asset_config_node_t* node, const cchar* path);
+
+    IFB_ENG_INTERNAL asset_config_list_t*           asset_config_list_arena_alloc                     (eng_mem_arena_t*);
+    IFB_ENG_INTERNAL void                           asset_config_list_add_node                        (asset_config_list_t* file_list, asset_config_node_t* file_node);
+    IFB_ENG_INTERNAL void                           asset_config_list_remove_node                     (asset_config_list_t* file_list, asset_config_node_t* file_node);
+
+    IFB_ENG_INTERNAL asset_config_editor_context_t* asset_config_editor_context_arena_alloc           (eng_mem_arena_t* arena);
+    IFB_ENG_INTERNAL bool                           asset_config_editor_context_is_valid              (const asset_config_editor_context_t* editor_context);
+    IFB_ENG_INTERNAL void                           asset_config_editor_context_assert_valid          (const asset_config_editor_context_t* editor_context);
+    IFB_ENG_INTERNAL asset_config_node_t*           asset_config_editor_context_add_text              (asset_config_editor_context_t* editor_context, eng_mem_arena_t* arena, const cchar* asset_name, const cchar* asset_path);
+    IFB_ENG_INTERNAL asset_config_node_t*           asset_config_editor_context_add_image             (asset_config_editor_context_t* editor_context, eng_mem_arena_t* arena, const cchar* asset_name, const cchar* asset_path);
+    IFB_ENG_INTERNAL asset_config_node_t*           asset_config_editor_context_add_sound             (asset_config_editor_context_t* editor_context, eng_mem_arena_t* arena, const cchar* asset_name, const cchar* asset_path);
+    IFB_ENG_INTERNAL asset_config_node_t*           asset_config_editor_context_add_font              (asset_config_editor_context_t* editor_context, eng_mem_arena_t* arena, const cchar* asset_name, const cchar* asset_path);
+    IFB_ENG_INTERNAL void                           asset_config_editor_context_update_file_node_name (asset_config_editor_context_t* editor_context, asset_config_node_t* file_node, const cchar* asset_name);
+    IFB_ENG_INTERNAL void                           asset_config_editor_context_update_file_node_path (asset_config_editor_context_t* editor_context, asset_config_node_t* file_node, const cchar* asset_path);
+    IFB_ENG_INTERNAL void                           asset_config_editor_delete_file_node              (asset_config_editor_context_t* editor_context, asset_config_node_t* file_node);
+
+    struct asset_config_cstr_t {
+        cchar chars[]
+    };
+
+    struct asset_config_node_data_t {
         eng_asset_cstr_t* name;
         eng_asset_cstr_t* path;
+    };  
+
+    struct asset_config_node_t : sld::dl_node_t<asset_config_node_data_t> { }; 
+    struct asset_config_list_t : sld::dl_list_t<asset_config_node_data_t> {
+        asset_config_editor_context_t* editor_context;
     };
 
-    struct eng_asset_config_file_list_t {
-        eng_asset_config_file_node_t* first;
-        eng_asset_config_file_node_t* last;
-    };
-
-    struct eng_asset_config_editor_context_t {
+    struct asset_config_editor_context_t {
         eng_asset_config_cstr_t* path;
         struct {
-            eng_asset_config_file_list_t text;
-            eng_asset_config_file_list_t image;
-            eng_asset_config_file_list_t sound;
-            eng_asset_config_file_list_t font;
-        } file_list;
+            asset_config_list_t* text;
+            asset_config_list_t* image;
+            asset_config_list_t* sound;
+            asset_config_list_t* font;
+            asset_config_list_t* deleted;
+        } node_list;
     };
 
-    struct eng_asset_config_file_node_t {
-        eng_asset_cstr_t*              name;
-        eng_asset_cstr_t*              path;
-        eng_asset_config_file_node_t*  next;
-        eng_asset_config_file_node_t*  prev;
-    };
-
-    
-    IFB_ENG_INTERNAL_INLINE eng_asset_config_file_node_t* 
+    IFB_ENG_INTERNAL_INLINE asset_config_node_t* 
     asset_config_arena_alloc_file_node(eng_mem_arena_t* arena) {
 
-        auto file_node    = sld::arena_push_struct<eng_asset_config_file_node_t> (arena);
-        auto string_array = sld::arena_push_struct<eng_asset_cstr_t>             (arena, 2);
+        auto node         = sld::arena_push_struct<asset_config_node_t> (arena); 
+        auto string_array = sld::arena_push_struct<eng_asset_cstr_t>    (arena, 2);
         
-        assert(
-            file_node    != NULL &&
-            string_array != NULL
-        );
+        assert(node != NULL && string_array != NULL);
 
-        file_node->name = &string_array[0];
-        file_node->path = &string_array[1];
-        file_node->next = NULL;
-        file_node->prev = NULL;
-        return(file_node);
+        node->data.name = &string_array[0];
+        node->data.path = &string_array[1];
+        return(node);
     };
 
-    IFB_ENG_INTERNAL_INLINE eng_asset_config_file_node_t*
+    IFB_ENG_INTERNAL_INLINE asset_config_node_t*
     asset_config_arena_alloc_and_init_file_node(
         eng_mem_arena_t* arena,
         const cchar*     asset_name,
@@ -94,25 +104,24 @@ namespace ifb {
 
         assert(can_init);
 
-        eng_asset_config_file_node_t* node = asset_config_arena_alloc_file_node(arena);
-        asset_config_file_node_update_name (node, asset_name);
-        asset_config_file_node_update_path (node, asset_path);
-
+        asset_config_node_t* node = asset_config_arena_alloc_file_node(arena);
+        asset_config_node_update_name (node, asset_name);
+        asset_config_node_update_path (node, asset_path);
         return(node);
     }
 
     IFB_ENG_INTERNAL_INLINE void
-    asset_config_file_node_update_name(
-        eng_asset_config_file_node_t* file_node,
-        const cchar*                  name_src_cchar) {
+    asset_config_node_update_name(
+        asset_config_node_t* node,
+        const cchar*         name_src_cchar) {
 
         assert(
-            file_node       != NULL &&
-            file_node->name != NULL &&
-            name_src_cchar  != NULL
+            node           != NULL &&
+            node->data.name     != NULL &&
+            name_src_cchar != NULL
         );
 
-        eng_asset_cstr_t* name = file_node->name;
+        eng_asset_cstr_t* name = node->data.name;
         cstr_t    dst          = { name->chars, IFB_ENG_ASSET_CSTR_SIZE};
         const u32 bytes_copied = sld::cstr_copy_from(&dst, name_src_cchar, IFB_ENG_ASSET_CSTR_SIZE);
 
@@ -121,49 +130,40 @@ namespace ifb {
             bytes_copied <= IFB_ENG_ASSET_CSTR_SIZE
         );
 
-        eng_asset_name_sanitize(1, file_node->name);
+        eng_asset_name_sanitize(1, node->data.name);
     }
 
     IFB_ENG_INTERNAL_INLINE void
     asset_config_file_list_add_node(
-        eng_asset_config_file_list_t* file_list,
-        eng_asset_config_file_node_t* file_node) {
+        asset_config_list_t* file_list,
+        asset_config_node_t* file_node) {
 
         assert(file_list != NULL && file_node != NULL);
 
-        if (file_list->first == NULL) {
-            assert(file_list->last != NULL);
-
-            file_node->next = NULL;
-            file_node->prev = NULL;
-            file_list->first = file_node;
-            file_list->last  = file_node;
-            return;
-        }
-
-        assert(file_list->first != NULL && file_list->last != NULL);
-
-        eng_asset_config_file_node_t* old_last = file_list->last;
-
-        file_node->next = NULL;
-        file_node->prev = old_last;
-        old_last->next  = file_node;
-        file_list->last = file_node;
-        assert(old_last->prev != NULL);
+        const bool did_insert = sld::dl_list_insert_at_tail(file_list, file_node);
+        assert(did_insert);
     }
 
     IFB_ENG_INTERNAL_INLINE void
-    asset_config_file_node_update_path(
-        eng_asset_config_file_node_t* file_node,
-        const cchar*                  path_src_cchar) {
+    asset_config_file_list_remove_node(
+        asset_config_list_t* file_list,
+        asset_config_node_t* file_node) {
+        
+        
+    }
+
+    IFB_ENG_INTERNAL_INLINE void
+    asset_config_node_update_path(
+        asset_config_node_t* file_node,
+        const cchar*         path_src_cchar) {
 
         assert(
-            file_node       != NULL &&
-            file_node->path != NULL &&
-            path_src_cchar  != NULL
+            file_node            != NULL &&
+            file_node->data.path != NULL &&
+            path_src_cchar       != NULL
         );
 
-        eng_asset_cstr_t* path = file_node->path;
+        eng_asset_cstr_t* path = file_node->data.path;
         cstr_t    dst          = { path->chars, IFB_ENG_ASSET_CSTR_SIZE};
         const u32 bytes_copied = sld::cstr_copy_from(&dst, path_src_cchar, IFB_ENG_ASSET_CSTR_SIZE);
 
