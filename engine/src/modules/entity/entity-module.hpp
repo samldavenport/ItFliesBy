78d7b6192d95356bd12204aab73ec82f789c4e17
thereclaimer <sam.davenport@thereclaimergame.com>
2026-01-05 09:49:16 -0600

    hashing out entity module structure

--- engine/src/modules/entity/entity-module.hpp
+++ engine/src/modules/entity/entity-module.hpp
@@ -2,9 +2,31 @@
 #define ENTITY_MODULE_HPP
 
 #include "ifb-engine.hpp"
+#include "managers.hpp"
 
 namespace ifb::eng {
 
+    //-------------------------------------------------------------------
+    // TYPES
+    //-------------------------------------------------------------------
+
+    struct entity_module;
+
+    //-------------------------------------------------------------------
+    // METHODS
+    //-------------------------------------------------------------------
+
+    IFB_ENG_INTERNAL entity_module* entity_module_init     (void);
+    IFB_ENG_INTERNAL void           entity_module_startup  (void);
+    IFB_ENG_INTERNAL void           entity_module_shutdown (void);
+
+    //-------------------------------------------------------------------
+    // DEFINITIONS
+    //-------------------------------------------------------------------
+
+    struct entity_module {
+        entity_manager* entity_manager;
+    };
 };
 
 #endif //ENTITY_MODULE_HPP
